eee9eff1f561e954e2f96c48a1f9cc16ada6f539
Luan Novais <luaannovais@live.com>
2021-11-29 21:42:49 -0300

    Feat: add files

--- src/hooks/useSearchContext.ts
+++ src/hooks/useSearchContext.ts
@@ -1,3 +1,10 @@
-const useSearchContext = () => {};
+import { useContext } from 'react';
+import { SearchContext } from '../contexts/SearchContext';
+
+const useSearchContext = () => {
+  const context = useContext(SearchContext);
+
+  return context;
+};
 
 export default useSearchContext;
